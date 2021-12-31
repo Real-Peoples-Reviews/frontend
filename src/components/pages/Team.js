f28b18b2c8b96b09055aee0deff1ef6875bf6e3b
Rick <ricksmycodeguy@gmail.com>
2021-12-31 17:15:11 -0600

    corrects misspelling of classNames to className

--- src/components/pages/Team.js
+++ src/components/pages/Team.js
@@ -161,13 +161,13 @@ const Team = () => {
             </StyledHeader>
             <StyledCards className="cardsContainer">
                 <div className="card">
-                    <img classNames="card-photo" src={rick} alt="Rick Mansfield" />
+                    <img className="card-photo" src={rick} alt="Rick Mansfield" />
                     <h2>Rick Mansfield</h2>
                     <p>Full Stack Developer</p>
                 </div>
 
                 <div className="card">
-                    <img classNames="card-photo" src={chris} alt="Jared Hall" />
+                    <img className="card-photo" src={chris} alt="Jared Hall" />
                     <h2>Chris Boyle</h2>
                     <p>Full Stack Developer</p>
                 </div>
