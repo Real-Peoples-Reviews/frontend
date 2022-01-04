47b17e268e013306c8a9fdc57764576b66b7254c
Teresa Franxman <teresafranxman97@gmail.com>
2022-01-04 11:54:25 -0600

    Validation updated, form now displays error messages

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,74 +1,89 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { Formik } from "formik";
 import schema from "../../schemas/schema";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Login = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
-	} = useForm({
-		resolver: yupResolver(schema),
-	});
-
-	const onHandleSubmission = (e) => {
-		console.log(e);
-		reset();
-	};
 
 	return (
-		<div className="container space-2">
-			<form onSubmit={handleSubmit(onHandleSubmission)}>
-				<div className="form-group">
-					<div className="heading">
-						<h2>
-							Welome
-							<span> back </span>
-						</h2>
-						<p>Sign in to manage your account.</p>
-					</div>
-					<label>
-						EMAIL ADDRESS
-						<input {...register("email")} placeholder="email" type="text" required />
-					</label>
+		<Formik
+			validationSchema={schema}
+			onSubmit={(e) => {
+				console.log(e)
+
+			}}
+			initialValues={{
+				email: "",
+				password: "",
+			}}
+		>
+			{({
+				handleSubmit,
+				handleChange,
+				handleReset,
+				values,
+				touched,
+				isValid,
+				errors,
+			}) => (
+				<div className="container space-2">
+					<Form onSubmit={handleSubmit} >
+						<Form.Group className="form-group">
+							<div className="heading">
+								<h2>
+									Welome
+									<span> back </span>
+								</h2>
+								<p>Sign in to manage your account.</p>
+							</div>
+							<Form.Label>EMAIL ADDRESS</Form.Label>
+							<Form.Control
+								required
+								type="email"
+								name="email"
+								value={values.email}
+								onChange={handleChange}
+								isValid={touched.email && !errors.email}
+							/>
+						</Form.Group>
+						<Form.Group className="form-group">
+							<Form.Label>
+								<span>PASSWORD</span>
+							</Form.Label>
+							<Link to="/">
+								<span> Forgot your password? </span>
+							</Link>
+							<Form.Control
+								required
+								type="text"
+								name="password"
+								value={values.password}
+								onChange={handleChange}
+								isValid={touched.password && !errors.password}
+							/>
+						</Form.Group>
+						<Form.Group className="form-group">
+							<div className="custom-checkbox">
+								<Form.Check name="remember" label="Remember Me" />
+							</div>
+						</Form.Group>
+						<div className="mb-5">
+							<div className="col-6">
+								<span className="small text-muted">Don't have an account?</span>
+								<Link to="/register">Sign Up</Link>
+							</div>
+							<div className="col-6 text-right">
+								<Button type="submit" variant="primary" className="btn"  >
+									Sign In
+								</Button>
+							</div>
+						</div>
+					</Form>
 				</div>
-				<div className="form-group">
-					<label>
-						<span>PASSWORD</span>
-					</label>
-					<Link to="/">
-						<span> Forgot your password? </span>
-					</Link>
-					<input {...register("password")} placeholder="password" type="password" required />
-				</div>
-				<div className="form-group">
-					<div className="custom-checkbox">
-						<input
-							type="checkbox"
-							name="remember"
-							className="custom-control-input"
-						/>
-						<label for="remember" className="custom-control-label">
-							Remember Me
-						</label>
-					</div>
-				</div>
-				<div className="mb-5">
-					<div className="col-6">
-						<span className="small text-muted">Don't have an account?</span>
-						<Link to="/register">Sign Up</Link>
-					</div>
-					<div className="col-6 text-right">
-						<button type="submit" className="btn">
-							Sign In
-						</button>
-					</div>
-				</div>
-			</form>
-		</div>
+			)}
+		</Formik>
 	);
 };
 
