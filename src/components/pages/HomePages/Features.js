cde1a3d4654037b9cdcfebfdb81feef0d8e8ff41
Rick <ricksmycodeguy@gmail.com>
2022-01-10 13:16:09 -0600

    adds header, footer, and sidebar to /features

--- src/components/pages/HomePages/Features.js
+++ src/components/pages/HomePages/Features.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Header from '../../common/Header';
+import Footer from '../../common/footer';
+import Sidebar from './Sidebar';
 
 const Features = () => {
+    let token = localStorage.getItem('token')
+    const [loggedIn, setLoggedIn] = useState(false);
     return (
         <div>
-            <h1>COOL FEATURES COMING SOON</h1>
+            <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} token={token} />
+            <div>
+                <h1>COOL FEATURES COMING SOON</h1>
+            </div>
+            <Sidebar />
+
+            <Footer />
         </div>
     )
 }
