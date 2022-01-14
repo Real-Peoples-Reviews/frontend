c0a3ddcf8fe4120fc37e5c361db46b450a685495
courtneyharris <harriscourtney98@gmail.com>
2022-01-14 18:59:10 -0500

    Adding content and styling has now begun

--- src/components/pages/HomePages/HowItWorks.js
+++ src/components/pages/HomePages/HowItWorks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import Header from "../../common/Header";
 import Sidebar from "../../pages/HomePages/Sidebar";
 
@@ -6,7 +7,19 @@ const HowItWorks = () => {
   return (
     <>
       <Header />
+      <div className="global-body">
       <Sidebar />
+      
+      <div>
+      <h1> How It Works </h1>
+      
+
+      <p>
+        You Join Of course, you'll need an account and to make some fast easy 
+        setting choices. It's private and secure. We do not sell to third parties.
+      </p>
+      </div>
+      </div>
     </>
   );
 };
