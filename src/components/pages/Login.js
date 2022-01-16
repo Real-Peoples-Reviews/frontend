f9340c62214a697608c41f720a9e3faef185c873
Rick <ricksmycodeguy@gmail.com>
2022-01-16 13:19:35 -0600

    wip

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { CDBCard, CDBBtn, CDBContainer, CDBCardBody, CDBIcon } from "cdbreact";
 import { Link, useNavigate } from "react-router-dom";
 import { Formik } from "formik";
-import axiosWithAuth from "../../utils/axiosWithAuth";
 import schema from "../../schemas/schema";
 import Form from "react-bootstrap/Form";
-import { CDBCard, CDBBtn, CDBContainer, CDBCardBody, CDBIcon } from "cdbreact";
 import Header from "../common/Header";
 import Footer from "../common/footer";
 import Sidebar from "./HomePages/Sidebar";
+import axios from "axios"
+// import axiosWithAuth from "../../utils/axiosWithAuth";
 
 
 const Login = () => {
@@ -17,117 +18,117 @@ const Login = () => {
     <div>
       <Header />
       <div className="global-body">
-          <Sidebar />
-    <Formik
-      validationSchema={schema}
-      onSubmit={(e) => {
-        axiosWithAuth()
-          .post("/users/login", e)
-          .then((res) => {
-            localStorage.setItem("token", res.data.token);
-            navigate("/home");
-          })
-          .catch((err) => {
-            console.log("Error:", err);
-          });
-      }}
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        handleReset,
-        values,
-        touched,
-        isValid,
-        errors,
-        }) => (
-          <div>
+        <Sidebar />
+        <Formik
+          validationSchema={schema}
+          onSubmit={(e) => {
+            axios()
+              .post("http://localhost:8000/users/login", e)
+              .then((res) => {
+                localStorage.setItem("token", res.data.token);
+                navigate("/home");
+              })
+              .catch((err) => {
+                console.log("Error:", err);
+              });
+          }}
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleReset,
+            values,
+            touched,
+            isValid,
+            errors,
+          }) => (
+            <div>
 
-        <CDBContainer className="container space-2">
-          <CDBCard style={{ width: "30rem" }}>
-            <CDBCardBody className="mx-4">
-              <Form onSubmit={handleSubmit}>
-                <Form.Group className="form-group">
-                  <div className="text-center mt-4 mb-2" id="heading">
-                    <p className="h4">
-                      Welome
-                      <span> back </span>
-                    </p>
-                    <p>Sign in to manage your account.</p>
-                  </div>
-                  <Form.Control
-                    required
-                    type="email"
-                    name="email"
-                    placeholder="E-mail"
-                    value={values.email}
-                    onChange={handleChange}
-                    isValid={touched.email && !errors.email}
-                  />
-                </Form.Group>
+              <CDBContainer className="container space-2">
+                <CDBCard style={{ width: "30rem" }}>
+                  <CDBCardBody className="mx-4">
+                    <Form onSubmit={handleSubmit}>
+                      <Form.Group className="form-group">
+                        <div className="text-center mt-4 mb-2" id="heading">
+                          <p className="h4">
+                            Welome
+                            <span> back </span>
+                          </p>
+                          <p>Sign in to manage your account.</p>
+                        </div>
+                        <Form.Control
+                          required
+                          type="email"
+                          name="email"
+                          placeholder="E-mail"
+                          value={values.email}
+                          onChange={handleChange}
+                          isValid={touched.email && !errors.email}
+                        />
+                      </Form.Group>
 
-                <Form.Group className="form-group">
-                  <Form.Control
-                    required
-                    type="text"
-                    name="password"
-                    placeholder="Password"
-                    value={values.password}
-                    onChange={handleChange}
-                    isValid={touched.password && !errors.password}
-                  />
-                </Form.Group>
-                <Form.Group className="form-group">
-                  <div className="d-flex flex-wrap justify-content-center align-items-center">
-                    <Form.Check name="remember" />
-                    <p className="m-0">Remember me</p>
-                    <Link to="/">Forgot Password?</Link>
-                  </div>
-                </Form.Group>
+                      <Form.Group className="form-group">
+                        <Form.Control
+                          required
+                          type="text"
+                          name="password"
+                          placeholder="Password"
+                          value={values.password}
+                          onChange={handleChange}
+                          isValid={touched.password && !errors.password}
+                        />
+                      </Form.Group>
+                      <Form.Group className="form-group">
+                        <div className="d-flex flex-wrap justify-content-center align-items-center">
+                          <Form.Check name="remember" />
+                          <p className="m-0">Remember me</p>
+                          <Link to="/">Forgot Password?</Link>
+                        </div>
+                      </Form.Group>
 
-                <div className="mb-5">
-                  <CDBBtn
-                    type="submit"
-                    color="dark"
-                    className="btn-block my-3 mx-0"
-                  >
-                    Sign In
-                  </CDBBtn>
-                </div>
-                <div>
-                  <p className="text-center">
-                    Not a member?
-                    <Link className="d-inline p-0" to="/signup">
-                      Register
-                    </Link>
-                  </p>
-                </div>
-                <div>
-                  <p className="text-center"> or sign in with</p>
-                  <CDBBtn color="white" style={{ boxShadow: "none" }}>
-                    <CDBIcon fab icon="facebook" />
-                  </CDBBtn>
-                  <CDBBtn color="white" style={{ boxShadow: "none" }}>
-                    <CDBIcon fab icon="twitter" />
-                  </CDBBtn>
-                  <CDBBtn color="white" style={{ boxShadow: "none" }}>
-                    <CDBIcon fab icon="google" />
-                  </CDBBtn>
-                </div>
-              </Form>
-            </CDBCardBody>
-          </CDBCard>
-        </CDBContainer>
-        </div>
-      )}
+                      <div className="mb-5">
+                        <CDBBtn
+                          type="submit"
+                          color="dark"
+                          className="btn-block my-3 mx-0"
+                        >
+                          Sign In
+                        </CDBBtn>
+                      </div>
+                      <div>
+                        <p className="text-center">
+                          Not a member?
+                          <Link className="d-inline p-0" to="/signup">
+                            Register
+                          </Link>
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-center"> or sign in with</p>
+                        <CDBBtn color="white" style={{ boxShadow: "none" }}>
+                          <CDBIcon fab icon="facebook" />
+                        </CDBBtn>
+                        <CDBBtn color="white" style={{ boxShadow: "none" }}>
+                          <CDBIcon fab icon="twitter" />
+                        </CDBBtn>
+                        <CDBBtn color="white" style={{ boxShadow: "none" }}>
+                          <CDBIcon fab icon="google" />
+                        </CDBBtn>
+                      </div>
+                    </Form>
+                  </CDBCardBody>
+                </CDBCard>
+              </CDBContainer>
+            </div>
+          )}
         </Formik>
-        </div>
-      <Footer />
       </div>
+      <Footer />
+    </div>
   );
 };
 
--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -1,8 +1,11 @@
 import * as yup from "yup";
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
 const schema = yup.object().shape({
     email: yup.string().email().required(),
     password: yup.string().required(),
+    phoneNumber: yup.string().matches(phoneRegExp, 'Phone number is not valid')
+
 });
 
 export default schema;
